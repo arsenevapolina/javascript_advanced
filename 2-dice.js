9fbe288d75e587138df917744e7230d0d05b8cf8
Polina <pechenkova.polina.mgu@yandex.ru>
2024-04-25 04:24:58 +0300

    иная реализация

--- 2-dice.js
+++ 2-dice.js
@@ -1,34 +1,24 @@
 "use strict";
 
 function gameDice(diceType) {
-  let min = 1;
-  let max;
+  const diceValues = {
+    d4: 4,
+    d6: 6,
+    d8: 8,
+    d10: 10,
+    d12: 12,
+    d16: 16,
+    d20: 20,
+  };
 
-  switch (diceType) {
-    case "d4":
-      max = 4;
-      break;
-    case "d6":
-      max = 6;
-      break;
-    case "d8":
-      max = 8;
-      break;
-    case "d10":
-      max = 10;
-      break;
-    case "d12":
-      max = 12;
-      break;
-    case "d16":
-      max = 16;
-      break;
-    case "d20":
-      max = 20;
-      break;
+  const max = diceValues[diceType];
+  const min = 1;
+
+  if (Number(max)) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    return "Сделайте новый бросок";
   }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 console.log(gameDice("d4"));
